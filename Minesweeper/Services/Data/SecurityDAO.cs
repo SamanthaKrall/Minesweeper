770d2c1883f5fe2d1b7297ce1f16b8508ac7c4cb
William Thornton <ironwillthornton@gmail.com>
2020-03-08 16:58:33 -0700

    Added registration page and all necessary styles

--- Minesweeper/Services/Data/SecurityDAO.cs
+++ Minesweeper/Services/Data/SecurityDAO.cs
@@ -13,8 +13,8 @@ namespace Minesweeper.Services.Data
 
         public bool FindByUser(Models.UserModel user)
         {
-            bool authenticate = true;
-            string queryString = "SELECT * FROM dbo.Users FROM USERNAME = @Username AND PASSWORD = @Password";
+            bool authenticate = false;
+            string queryString = "SELECT * FROM dbo.Users WHERE USERNAME = @Username AND PASSWORD = @Password";
             using (System.Data.SqlClient.SqlConnection conn = new SqlConnection(connection))
             {
                 SqlCommand command = new SqlCommand(queryString, conn);
@@ -36,5 +36,36 @@ namespace Minesweeper.Services.Data
 
             return authenticate;
         }
+
+        public bool NewUser(Models.UserModel user)
+        {
+            //set outcome to false because return statement should default to false not true
+            bool outcome = false;
+            string queryString = "INSERT INTO dbo.Users(USERNAME, PASSWORD) VALUES(@Username, @Password)";
+            using (System.Data.SqlClient.SqlConnection conn = new SqlConnection(connection))
+            {
+                SqlCommand command = new SqlCommand(queryString, conn);
+                command.Parameters.Add("@USERNAME", System.Data.SqlDbType.VarChar, 50).Value = user.Username;
+                command.Parameters.Add("@PASSWORD", System.Data.SqlDbType.VarChar, 50).Value = user.Password;
+                try
+                {
+                    conn.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    adapter.InsertCommand = (command);
+                    adapter.InsertCommand.ExecuteNonQuery();
+                    command.Dispose();
+                    conn.Close();
+                    outcome = true;
+                    return outcome;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return outcome;
+        }
     }
+
+
 }
